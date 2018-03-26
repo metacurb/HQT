caca9ab8a27f10f37ca675aac788e4a9442ff5bf
Beau August <beauagst@gmail.com>
2018-03-26 15:41:28 +0100

    chore: Update .gitignore Removed dist folder from being committed

--- dist/reader/scan.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var scan = {};
-
-exports.default = scan;
--- dist/resolvers/wikipedia.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var wikipedia = {};
-
-exports.default = wikipedia;
