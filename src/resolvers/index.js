f16ff01a828f3b84aae9a5158601ba0b76911d55
Beau August <beauagst@gmail.com>
2018-03-27 14:03:55 +0100

    babel: chore Added object spread operator

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,14 +1,13 @@
-import chalk from 'chalk';
 import google from './google';
 import wikipedia from './wikipedia';
-import wolfram from './wolfram';
+// import wolfram from './wolfram';
 
 const init = async ({ question, answers }) => {
   const results = {};
   await Promise.all(answers.map(async (answer) => {
     results[answer] = {
-      google: await google(`${question} ${answer}`),
-      wikipedia: await wikipedia(question, answer),
+      ...await google(question, answer),
+      ...await wikipedia(question, answer),
     };
   }));
   return results;
