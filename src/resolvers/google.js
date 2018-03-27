9c66335af22c7841756b3fcbbaad0d7f31702d5f
Beau August <beauagst@gmail.com>
2018-03-27 14:05:45 +0100

    chore: Google resolver Updated function to return useful data

--- src/resolvers/google.js
+++ src/resolvers/google.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
+import helpers from '../helpers';
 
-export default async function google(query) {
-  const url = `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_KEY}&cx=${process.env.GOOGLE_ENGINE_ID}&q=${query}`;
+export default async function google(question, answer) {
+  const url = `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_KEY}&cx=${process.env.GOOGLE_ENGINE_ID}&q=${question} ${answer}`;
   try {
     const response = await axios.get(url);
+    const snippetText = response.data.items.reduce((text, item) => `${text}${item.snippet}`, '');
     return {
       numResults: response.data.searchInformation.formattedTotalResults,
-      items: [
-        response.data.items[0].snippet,
-        response.data.items[1].snippet,
-      ],
+      googleHits: helpers.getHits(question, snippetText),
     };
   } catch (err) {
     return Promise.reject();
