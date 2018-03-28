15ef4e2bef239e25a71d018ae323e1ab21a6eb9a
Beau August <beauagst@gmail.com>
2018-03-28 16:20:54 +0100

    Removed bearer token

--- src/reader/scan.js
+++ src/reader/scan.js
@@ -5,7 +5,7 @@ export default function scan() {
   // const ws = new WebSocket('https://ws-quiz.hype.space/ws/44250');
   const ws = new WebSocket('https://ws-quiz.hype.space/ws/44250', {
     headers: {
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE0MjIyODI0LCJ1c2VybmFtZSI6IkJlYXVndXN0IiwiYXZhdGFyVXJsIjoiczM6Ly9oeXBlc3BhY2UtcXVpei9hL2U0LzE0MjIyODI0LVh3OUl5aS5qcGciLCJ0b2tlbiI6IkRSeDNtSSIsInJvbGVzIjpbXSwiY2xpZW50IjoiIiwiZ3Vlc3RJZCI6bnVsbCwidiI6MSwiaWF0IjoxNTIyMTc4MzczLCJleHAiOjE1Mjk5NTQzNzMsImlzcyI6Imh5cGVxdWl6LzEifQ.6Ji3UlINy8j0yLYbNa77Iie8jW9qr07LoLR4tQ-MMYI',
+      Authorization: 'Bearer ', // Place bearer token in here!
     },
   });
 
