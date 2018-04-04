071d2372d53313ffdc89bad90d14589d8919e6de
unknown <beauagst@gmail.com>
2018-04-04 21:05:39 +0100

    fix: Update hit counter Counts all occurences of a word, rather than just one

--- src/helpers/getHits.js
+++ src/helpers/getHits.js
@@ -1,9 +1,7 @@
 export default function getHits(question, text) {
   const words = question.split(' ');
-  let hits = 0;
-  words.forEach((word) => {
-    if (text.toLowerCase().indexOf(word) === -1) return;
-    hits += 1;
-  });
-  return hits;
+  return words.reduce((hits, word) => {
+    const re = new RegExp(word, 'g');
+    return hits + (text.match(re) || []).length;
+  }, 0);
 }
