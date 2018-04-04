e0b38a389f60fb9982dae160f40ff516064bb5fa
unknown <beauagst@gmail.com>
2018-04-04 21:07:03 +0100

    feature: Google resolver Now allows query without question

--- src/resolvers/google.js
+++ src/resolvers/google.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import helpers from '../helpers';
 
-export default async function google(question, answer) {
-  const url = `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_KEY}&cx=${process.env.GOOGLE_ENGINE_ID}&q=${question} ${answer}`;
+export default async function google(question, answer = false) {
+  const queryString = answer ? `${question} ${answer}` : question;
+  const url = `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_KEY}&cx=${process.env.GOOGLE_ENGINE_ID}&q=${queryString}`;
   try {
     const response = await axios.get(url);
-    const snippetText = response.data.items.reduce((text, item) => `${text}${item.snippet}`, '');
+    const snippetText = response.data.items.reduce((text, item) => `${text}${item.snippet}`, '').toLowerCase();
+    if (!answer) return snippetText;
     return {
       numResults: helpers.formatNumber(response.data.searchInformation.formattedTotalResults),
       hits: helpers.getHits(question, snippetText),
