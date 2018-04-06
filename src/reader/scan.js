f1a6609acab90b8ecda78b59ff47272ded8d86c9
Beau August <beauagst@gmail.com>
2018-04-06 15:15:19 +0100

    feature: Added heartbeat to socket Stored more questions

--- src/reader/scan.js
+++ src/reader/scan.js
@@ -1,3 +1,4 @@
+import { setWsHeartbeat } from 'ws-heartbeat/client';
 import WebSocket from 'ws';
 import fs from 'fs';
 import parse from './parse';
@@ -15,15 +16,17 @@ const storeQuestion = (question) => {
 export default function scan(socketUrl) {
   const ws = new WebSocket(socketUrl, {
     headers: {
-      // Connection: 'Keep-Alive',
-      // Host: 'api-quiz.hype.space',
       Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
-      // 'Accept-Encoding': 'gzip',
       'User-Agent': 'okhttp/3.8.0',
       'x-hq-client': 'Android/1.5.1',
       'x-hq-lang': 'en',
       'x-hq-stk': 'MQ==',
     },
+  });
+
+  setWsHeartbeat(ws, '{"kind":"ping"}', {
+    pingTimeout: 1200000,
+    pingInterval: 25000,
   });
 
   ws.on('open', () => {
@@ -36,6 +39,11 @@ export default function scan(socketUrl) {
 
   ws.on('message', async (message) => {
     const data = JSON.parse(message);
+    if (data.type === 'broadcastEnded') {
+      log.warn('Game over, broadcast closed');
+      ws.close();
+    }
+    console.log(data.type);
     if (data.type !== 'question') return;
     log.success('Question found, resolving... \n');
     const question = parse(data);
